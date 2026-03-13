8b9800dcfdd63452aa8474fd26fb7a21cafc74f3
uuuumy <e.idanova@g.nsu.ru>
2026-03-13 23:26:07 +0700

    8.10.py

--- 8.10.py
+++ 8.10.py
@@ -1,6 +1,6 @@
-txt=input()
-txt_new=txt.split()
-list=[]
+txt = input()
+txt_new = txt.split()
+list = []
 for word in txt_new:
     if word != txt_new[0] and len(set(word)) == len(word):
         list.append(word)
