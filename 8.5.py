90487d3d82b9544be477df633fbbd84ee6011ee0
uuuumy <e.idanova@g.nsu.ru>
2026-03-13 23:31:05 +0700

    8.5.py

--- 8.5.py
+++ 8.5.py
@@ -1,7 +1,9 @@
-str1=input()
-str2=input()
-str3=input()
-allstr=list(set(str1)) + list(set(str2)) + list(set(str3))
-result=[i for i in allstr if allstr.count(i)==1]
+str1 = input()
+str2 = input()
+str3 = input()
+
+allstr = list(set(str1)) + list(set(str2)) + list(set(str3))
+result = [i for i in allstr if allstr.count(i) == 1]
+
 print(''.join(result))
 
